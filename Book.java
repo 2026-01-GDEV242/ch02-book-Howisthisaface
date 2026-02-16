9333303ab2c7810de9ea17800e7893cc42ba3572
Howisthisaface <60188690+Howisthisaface@users.noreply.github.com>
2026-02-16 12:43:00 -0500

    Update the setter

--- Book.java
+++ Book.java
@@ -3,7 +3,7 @@
  * This might form part of a larger application such
  * as a library system, for instance.
  *
- * @author (Zachary Robinson
+ * @author Zachary Robinson
  * @version 2/15/2026
  */
 import java.util.Scanner;
@@ -22,11 +22,11 @@ class Book
      * Set the author and title fields when this object
      * is constructed.
      */
-    public Book(String bookAuthor, String bookTitle)
+    public Book(String bookAuthor, String bookTitle, int pages, boolean courseText)
     {
         author = bookAuthor;
         title = bookTitle;
-        pages = 0;
+        this.pages = pages;
         refNumber = "";
         this.courseText = courseText;
     }
@@ -36,7 +36,14 @@ class Book
 
     public void setRefNumber(String ref)
     {
-        setRefNumber(refNumber);
+        if(ref.length() <= 3)
+        {
+            System.out.println("Error. Please input three or more characters.");
+        }
+        else
+        {
+            refNumber = ref;
+        }
     }
     
     public String getRefNumber()
@@ -50,6 +57,7 @@ class Book
             System.out.println("Error. Please input three or more characters.");
             refNumber = "";
         }
+        return refNumber;
     }
     
     public void getBorrowed()
@@ -68,7 +76,7 @@ class Book
         System.out.println(", Author: " + author);
         System.out.println(", Pages: " + pages);
         
-        if (refNumber != null && refNumber() > 0) 
+        if (refNumber.length() > 0) 
         {
             System.out.println("The reference number is: " + refNumber);
         }
